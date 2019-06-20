ac5d8337f740f23947d1c94f2f8dbf6444b8b50e
Roman <tsvetkovv22@gmail.com>
2019-06-20 23:48:14 +1000

    Ознакомительная работа с формой

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="name"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
   res.write('<body><h1>My first page</h1></body>');
   res.write('</html>');
   res.end();
-  if (req.url === '/exit') {
-    process.exit();
-  }
 });
 
 server.listen(3001, () => console.log('Server runing on 3001 port!'));
