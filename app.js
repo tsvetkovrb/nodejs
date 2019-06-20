d9e279bc590f52a7631d872c0d4d6b0a1b53fa79
Roman <tsvetkovv22@gmail.com>
2019-06-20 23:41:01 +1000

    Работа с response

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first page</title></head>');
+  res.write('<body><h1>My first page</h1></body>');
+  res.write('</html>');
+  res.end();
+  if (req.url === '/exit') {
+    process.exit();
+  }
 });
 
-server.listen(3000, () => console.log('Server runing on 3000 port!'));
+server.listen(3001, () => console.log('Server runing on 3001 port!'));
