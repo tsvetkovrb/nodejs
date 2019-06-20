3415edb8c5016ece53b615f048badcaadf5a764e
Roman <tsvetkovv22@gmail.com>
2019-06-21 00:09:27 +1000

    Работа с файлом

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -12,6 +14,25 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     return res.end();
   }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.writeHead(302, {
+      Location: '/'
+    });
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
