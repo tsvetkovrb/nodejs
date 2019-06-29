e4421987d3096c8f34e5cec31e764a88efc627fd
Roman <tsvetkovv22@gmail.com>
2019-06-29 23:53:56 +1000

    Adding 404 Error Page

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('Page not found');
+});
+
 app.listen(3001, () => console.log('Server is listen on port 3001'));
