e1c4b781169c71bd01a5c16281b4a9664c242a25
Roman <tsvetkovv22@gmail.com>
2019-09-11 21:37:26 +0300

    Checking For Field Equality

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const router = express.Router();
 
@@ -9,7 +9,30 @@ router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email addres is forbidden');
+        }
+        return true;
+      }),
+    body('password', 'Please, enter a valid password')
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords have to match');
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup,
+);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
 router.get('/reset/:token', authController.getNewPassword);
