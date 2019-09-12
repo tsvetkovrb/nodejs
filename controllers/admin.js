6100bc2be6c874b48ff01d4efba0fe592c6b28f8
Roman <tsvetkovv22@gmail.com>
2019-09-13 00:01:44 +0300

    Validating Product Editing

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator/check');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -5,6 +6,9 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add product',
     path: '/admin/add-product',
     editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: [],
   });
 };
 
@@ -22,11 +26,14 @@ exports.getEditProduct = (req, res, next) => {
       if (!product) {
         return res.redirect('/');
       }
-      res.render('admin/edit-product', {
+      return res.render('admin/edit-product', {
         pageTitle: 'Edit product',
         path: '/admin/edit-product',
         editing: editMode,
         product,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: [],
       });
     })
     .catch(error => console.log(error));
@@ -34,6 +41,26 @@ exports.getEditProduct = (req, res, next) => {
 
 exports.postEditProduct = (req, res, next) => {
   const { title, imageUrl, price, description, editProductId } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit product',
+      path: '/admin/edit-product',
+      editing: true,
+      hasError: true,
+      product: {
+        title,
+        imageUrl,
+        price,
+        description,
+        _id: editProductId,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
 
   Product.findById(editProductId)
     .then(product => {
@@ -55,6 +82,23 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res) => {
   const { title, imageUrl, price, description } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add product',
+      path: '/admin/edit-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title,
+        imageUrl,
+        price,
+        description,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
   const product = new Product({
     price,
     title,
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator/check');
 
 const admminController = require('../controllers/admin');
 const checkIsLoggedIn = require('../middlewares/checkIsLoggedIn');
 
 router.get('/add-product', checkIsLoggedIn, admminController.getAddProduct);
-router.post('/add-product', checkIsLoggedIn, admminController.postAddProduct);
+router.post(
+  '/add-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 }),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 5, max: 120 }),
+  ],
+  checkIsLoggedIn,
+  admminController.postAddProduct,
+);
 router.get('/products', checkIsLoggedIn, admminController.getProducts);
-router.get('/edit-product/:productId', checkIsLoggedIn, admminController.getEditProduct);
-router.post('/edit-product', checkIsLoggedIn, admminController.postEditProduct);
-router.post('/delete-product', checkIsLoggedIn, admminController.postDeleteProduct);
+router.get(
+  '/edit-product/:productId',
+  checkIsLoggedIn,
+  admminController.getEditProduct,
+);
+router.post(
+  '/edit-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 }),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description').isLength({ min: 5, max: 120 }),
+  ],
+  checkIsLoggedIn,
+  admminController.postEditProduct,
+);
+router.post(
+  '/delete-product',
+  checkIsLoggedIn,
+  admminController.postDeleteProduct,
+);
 
 exports.routes = router;
